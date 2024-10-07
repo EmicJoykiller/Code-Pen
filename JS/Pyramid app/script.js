8e978e23abe1e634bfc915c89af782f1970a5eb6
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-10-06 20:20:10 -0400

    Update script.js

--- JS/Pyramid app/script.js
+++ JS/Pyramid app/script.js
@@ -2,19 +2,9 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
-  return name;
+function padRow() {
+
 }
-function addTwoNumbers(num1, num2) {
-  return num1 + num2;
-}
-
-const sum = addTwoNumbers(5, 10);
-console.log(sum)
-
-
-const call = padRow();
-console.log(call);
 
 
 for (let i = 0; i < count; i = i + 1) {
