c3ae4d61ef2d74d7bcd5a9b9792bfaeef004ab46
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-10-07 18:02:49 -0400

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -1,41 +1,15 @@
-class OnlyEvens extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('Should I update?');
+const styles = {
+  color: "purple",
+  fontSize: 40,
+  border: "2px solid purple",
+}
+// Change code above this line
+class Colorful extends React.Component {
+  render() {
     // Change code below this line
-    if (nextProps.value % 2 == 0)
-    return true;
+    return (
+      <div style={styles}>Style Me!</div>
+    );
     // Change code above this line
   }
-  componentDidUpdate() {
-    console.log('Component re-rendered.');
-  }
-  render() {
-    return <h1>{this.props.value}</h1>;
-  }
-}
-
-class Controller extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0
-    };
-    this.addValue = this.addValue.bind(this);
-  }
-  addValue() {
-    this.setState(state => ({
-      value: state.value + 1
-    }));
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.addValue}>Add</button>
-        <OnlyEvens value={this.state.value} />
-      </div>
-    );
-  }
-}
+};
