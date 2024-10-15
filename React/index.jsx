40b5722b60d79991eb6c4f532cd570ceaec4c112
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-10-14 20:11:05 -0400

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -1,7 +1,19 @@
-const JSX = (
-  <div>
-    <h1>This is a block of JSX</h1>
-    <p>Here's a subtitle</p>
-    {/* */}
-  </div>
-);
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        <Vegetables />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+
+// Change code below this line
+ReactDOM.render(<TypesOfFood/>, document.getElementById('challenge-node'))
