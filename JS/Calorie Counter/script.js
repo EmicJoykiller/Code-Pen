cfe3440fde283e9f81133f4570002edaab2012f3
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-11-26 18:50:35 -0500

    Update script.js

--- JS/Calorie Counter/script.js
+++ JS/Calorie Counter/script.js
@@ -33,3 +33,12 @@ function addEntry() {
 }
 
 addEntryButton.addEventListener("click", addEntry);
+
+function getCaloriesFromInputs(list){
+    let calories = 0;
+    for(const item of list){
+        const currVal  = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+        if(invalidInputMatch){}
+    };
+};
