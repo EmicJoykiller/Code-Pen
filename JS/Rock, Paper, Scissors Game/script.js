90cab1348c51737a114e3811d89394700e83ca7f
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-12-09 08:08:49 -0500

    Update script.js

--- JS/Rock, Paper, Scissors Game/script.js
+++ JS/Rock, Paper, Scissors Game/script.js
@@ -1,5 +1,22 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-  
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
   }
-  console.log(getRandomComputerResult());
+  
+  function hasPlayerWonTheRound(player, computer) {
+    if (
+        (player === "Rock" && computer === "Scissors") ||
+        (player === "Scissors" && computer === "Paper") ||
+        (player === "Paper" && computer === "Rock")
+    ){
+        return true
+    }
+    else{
+        return false
+    }
+   
+  }
+  
+  console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+  console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
