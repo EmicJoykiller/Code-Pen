81e78778c68510635c49705825f36a3a60994168
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-12-28 00:33:25 -0500

    Update script.js

--- JS/Music Player/script.js
+++ JS/Music Player/script.js
@@ -102,8 +102,22 @@ const renderSongs = (array)=> {
     playlistSongs.innerHTML = songsHTML;
 }
 
-renderSongs(userData?.songs);
+renderSongs(sortSongs());
+
+const playSong = (id)=>{
+    const song = userData?.songs.find((song)=>id === song.id)
+};
 
 const sortSongs = ()=> {
-    userData?.songs.sort((a,b)=>{});
+    userData?.songs.sort((a,b)=>{
+        if(a.title < b.title){
+            return -1
+        }
+        if(a.title > b.title){
+            return 1
+        }
+        return 0
+    });
+
+    return userData?.songs;
 };
