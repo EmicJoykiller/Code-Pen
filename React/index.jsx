e726ec491ecfce7ef934c1a6573654c0745945a2
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-02-23 19:19:20 -0500

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -1,12 +1,31 @@
-const JSX = (
-  <div>
-    <h1></h1>
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <p></p>
-  </div>
-);
+  }
+  render() {
+    return (
+        <div>
+            { /* Change code below this line */ }
+            <Welcome />
+            { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          { /* Change code below this line */ }
+          <p>Hello, <strong></strong>!</p>
+          { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
