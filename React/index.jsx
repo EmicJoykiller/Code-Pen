b2afff3db662d6fc6b756d8791cfd4eb18dcb0b7
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-03-07 00:16:26 -0500

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -1,7 +1,20 @@
-const JSX = (
-  <div>
-    <h1>This is a block of JSX</h1>
-    <p>Here's a subtitle</p>
-    {/* */}
-  </div>
-);
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    // Change code below this line
+
+    // Change code above this line
+    return (
+      <div>
+        { /* Change code below this line */ }
+
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
