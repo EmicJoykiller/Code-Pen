ef9d5a5934852e59750ab30ed678fa35533b2159
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-03-10 16:41:53 -0400

    Update index.jsx

--- React & Redux/index.jsx
+++ React & Redux/index.jsx
@@ -1,14 +1,23 @@
 class DisplayMessages extends React.Component {
-    // Change code below this line
-    constructor(props){
-      super(props)
-      this.state = {
-        input: "",
-        messages: []
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
     }
-    // Change code above this line
-    render() {
-      return <div />
-    }
-  };
+  }
+  // Add handleChange() and submitMessage() methods here
+
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        { /* Render an input, button, and ul below this line */ }
+        <input onChange={handleChange} value={this.state.input}/>
+        <button></button>
+        <ul></ul>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
