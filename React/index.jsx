6624f1a2cd3b53a390d4713f00830acd9375712a
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-03-10 13:47:26 -0400

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -1,4 +1,38 @@
-// Define an action here:
-const action = {
-  type: 'LOGIN'
-}
+
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+    case 'LOGIN': 
+      return {
+        authenticated: true
+      }
+    case 'LOGOUT': 
+      return {
+        authenticated: false
+      }
+
+    default:
+      return state;
+
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
